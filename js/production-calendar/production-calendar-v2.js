2ea68afbf9eb01714161c2a6a52047ba40bd5ca3
Artyom Demchenko <vays3btro@gmail.com>
2024-03-30 23:52:58 -0700

    add production calendar

--- js/production-calendar/production-calendar-v2.js
+++ js/production-calendar/production-calendar-v2.js
@@ -19,21 +19,16 @@ var monthText = [
   "Ноябрь",
   "Декабрь"
 ];
-if (month == 10 || month == 11 || month == 12) {
+// if (month == 10 || month == 11 || month == 12) {
 
-} else {
-  month = `0` + `${month}`;
-}
-
-console.log(month);
-
+// } else {
+//   month = `0` + `${month}`;
+// }
 
 
 // Найдём текущий год и добавим его на страницу
 let yearEl = document.querySelector('.c-paginator__year');
 yearEl.textContent = year;
-
-// let dayEls = document.querySelectorAll('.c-cal__cel');
 
 var indexMonth = month;
 var addBtn = $(".js-event__add");
@@ -43,77 +38,82 @@ var winCreator = $(".js-event__creator");
 var inputDate = $(this).data();
 today = year + "-" + month + "-" + day;
 
+// console.log(month);
+// console.log(today);
 
-// ------ установите события по умолчанию -------
-function defaultEvents(dataDay,dataName,dataNotes,classTag){
-  var date = $('*[data-day='+dataDay+']');
-  // date.attr("data-name", dataName);
-  // date.attr("data-notes", dataNotes);
-  date.addClass("event");
-  date.addClass("event--" + classTag);
-}
 
 // ------ управление функциями -------
 
-// // кнопка текущего дня
-// todayBtn.on("click", function() {
-//   if (month < indexMonth) {
-//     var step = indexMonth % month;
-//     movePrev(step, true);
-//   } else if (month > indexMonth) {
-//     var step = month - indexMonth;
-//     moveNext(step, true);
-//   }
-// });
 
 //высветите значение текущего дня
-dataCel.each(function() {
+dataCel.each(function () {
   if ($(this).data("day") === today) {
     $(this).addClass("isToday");
     fillEventSidebar($(this));
   }
 });
 
-console.log(today);
 
-//создатель события window
-addBtn.on("click", function() {
+
+//создатель события
+addBtn.on("click", function () {
   winCreator.addClass("isVisible");
   $("body").addClass("overlay");
-  dataCel.each(function() {
+
+  dataCel.each(function () {
+
     if ($(this).hasClass("isSelected")) {
       today = $(this).data("day");
-      document.querySelector('input[type="date"]').value = today;
+      console.log(today);
+      document.querySelector('input[name="date"]').value = today;
     } else {
-      document.querySelector('input[type="date"]').value = today;
+      document.querySelector('input[name="date"]').value = today;
     }
   });
+
+  $(function () {
+    $(".js-hide-search").select2(
+      {
+        dropdownParent: $('#addEvent'),
+        minimumResultsForSearch: 1 / 0,
+      });
+  });
+
 });
-closeBtn.on("click", function() {
+closeBtn.on("click", function () {
   winCreator.removeClass("isVisible");
   $("body").removeClass("overlay");
 });
-saveBtn.on("click", function() {
-  var inputName = $("input[name=name]").val();
+saveBtn.on("click", function () {
   var inputDate = $("input[name=date]").val();
   var inputNotes = $("textarea[name=notes]").val();
   var inputTag = $("select[name=tags]")
     .find(":selected")
     .val();
-  
 
-  dataCel.each(function() {
+  dataCel.each(function () {
     if ($(this).data("day") === inputDate) {
-      if (inputName != null) {
-        $(this).attr("data-name", inputName);
-      }
       if (inputNotes != null) {
         $(this).attr("data-notes", inputNotes);
       }
       $(this).addClass("event");
       if (inputTag != null) {
-        if ($(this).hasClass('event')) 
-        $(this).addClass("event--" + inputTag);
+        if ($(this).hasClass('event')) {
+
+        }
+        if ($(this).hasClass("event--event")) {
+          $(this).removeClass("event--event");
+          $(this).addClass("event--" + inputTag);
+        } else if ($(this).hasClass("event--important")) {
+          $(this).removeClass("event--important");
+          $(this).addClass("event--" + inputTag);
+        } else if ($(this).hasClass("event--birthday")) {
+          $(this).removeClass("event--birthday");
+          $(this).addClass("event--" + inputTag);
+        } else {
+          $(this).addClass("event--" + inputTag);
+        }
+
       }
       fillEventSidebar($(this));
     }
@@ -122,23 +122,26 @@ saveBtn.on("click", function() {
   winCreator.removeClass("isVisible");
   $("body").removeClass("overlay");
   $("#addEvent")[0].reset();
+
+  $('.js-hide-search').select2('destroy');
+
 });
 
 //заполнить боковую панель информацией о событии
 function fillEventSidebar(self) {
   $(".c-aside__event").remove();
-  var thisName = self.attr("data-name");
   var thisNotes = self.attr("data-notes");
   var thisImportant = self.hasClass("event--important");
   var thisBirthday = self.hasClass("event--birthday");
-  var thisFestivity = self.hasClass("event--festivity");
   var thisEvent = self.hasClass("event");
-  
+
   switch (true) {
     case thisImportant:
       $(".c-aside__eventList").append(
         "<p class='c-aside__event c-aside__event--important'>" +
         "Выходной день" +
+        " <span> • " +
+        thisNotes +
         "</span></p>"
       );
       break;
@@ -146,6 +149,8 @@ function fillEventSidebar(self) {
       $(".c-aside__eventList").append(
         "<p class='c-aside__event c-aside__event--birthday'>" +
         "Праздничный день" +
+        " <span> • " +
+        thisNotes +
         "</span></p>"
       );
       break;
@@ -153,19 +158,29 @@ function fillEventSidebar(self) {
       $(".c-aside__eventList").append(
         "<p class='c-aside__event'>" +
         "Рабочий день" +
+        " <span> • " +
+        thisNotes +
         "</span></p>"
       );
       break;
-   }
+  }
 };
-dataCel.on("click", function() {
+
+for (let i = 0; i < dataCel.length; i++) {
+  if (!dataCel[i].hasAttribute('data-day')) {
+    dataCel[i].classList.add('visib');
+  }
+}
+
+
+dataCel.on("click", function () {
   var thisEl = $(this);
   var thisDay = $(this)
-  .attr("data-day")
-  .slice(8);
+    .attr("data-day")
+    .slice(8);
   var thisMonth = $(this)
-  .attr("data-day")
-  .slice(5, 7);
+    .attr("data-day")
+    .slice(5, 7);
 
   fillEventSidebar($(this));
 
@@ -176,78 +191,6 @@ dataCel.on("click", function() {
   thisEl.addClass("isSelected");
 
 });
-
-//функция для перемещения месяцев
-function moveNext(fakeClick, indexNext) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "-=100%"
-    });
-    $(".c-paginator__month").css({
-      left: "-=100%"
-    });
-    switch (true) {
-      case indexNext:
-        indexMonth += 1;
-        break;
-    }
-  }
-}
-function movePrev(fakeClick, indexPrev) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "+=100%"
-    });
-    $(".c-paginator__month").css({
-      left: "+=100%"
-    });
-    switch (true) {
-      case indexPrev:
-        indexMonth -= 1;
-        break;
-    }
-  }
-}
-
-//разбиение на страницы по месяцам
-function buttonsPaginator(buttonId, mainClass, monthClass, next, prev) {
-  switch (true) {
-    case next:
-      $(buttonId).on("click", function() {
-        if (indexMonth >= 2) {
-          $(mainClass).css({
-            left: "+=100%"
-          });
-          $(monthClass).css({
-            left: "+=100%"
-          });
-          indexMonth -= 1;
-        }
-        return indexMonth;
-      });
-      break;
-    case prev:
-      $(buttonId).on("click", function() {
-        if (indexMonth <= 11) {
-          $(mainClass).css({
-            left: "-=100%"
-          });
-          $(monthClass).css({
-            left: "-=100%"
-          });
-          indexMonth += 1;
-        }
-        return indexMonth;
-      });
-      break;
-  }
-}
-
-buttonsPaginator("#next", monthEl, ".c-paginator__month", false, true);
-buttonsPaginator("#prev", monthEl, ".c-paginator__month", true, false);
-
-//запустите функцию для установки текущего месяца
-moveNext(indexMonth - 1, false);
 
 //заполните боковую панель текущим днем
 $(".c-aside__num").text(day);
--- js/production-calendar/production-calendar.js
+++ js/production-calendar/production-calendar.js
@@ -1,4 +1,4 @@
-//global variables
+//глобальные переменные
 var monthEl = $(".c-main");
 var dataCel = $(".c-cal__cel");
 var dateObj = new Date();
@@ -20,131 +20,130 @@ var monthText = [
   "Декабрь"
 ];
 
-// console.log(day);
-// console.log(month);
-// console.log(year);
-console.log(monthEl);
-console.log(dataCel);
-
-let yearEl = document.querySelector('.c-paginator__year');
-yearEl.textContent = year;
-
-let dayEls = document.querySelectorAll('.c-cal__cel');
-
-// dayEls.forEach(element => {
-//   element.setAttribute('data-day', `${year}-${month}-${day}`);
-// });
-
-
 var indexMonth = month;
-// var todayBtn = $(".c-today__btn");
 var addBtn = $(".js-event__add");
 var saveBtn = $(".js-event__save");
 var closeBtn = $(".js-event__close");
 var winCreator = $(".js-event__creator");
 var inputDate = $(this).data();
-today = year + "-" + month + "-" + day;
+// today = year + "-" + month + "-" + day;
 
-
-// ------ set default events -------
-function defaultEvents(dataDay,dataName,dataNotes,classTag){
-  var date = $('*[data-day='+dataDay+']');
-  date.attr("data-name", dataName);
-  date.attr("data-notes", dataNotes);
-  date.addClass("event");
-  date.addClass("event--" + classTag);
+if (month < 10) {
+  today = `${year}-0${month}-${day}`;
+} else {
+  today = year + "-" + month + "-" + day;
 }
 
-// defaultEvents(today, 'YEAH!','Today is your day','important');
-// defaultEvents('2022-12-25', 'MERRY CHRISTMAS','A lot of gift!!!!','festivity');
-// defaultEvents('2022-05-04', "LUCA'S BIRTHDAY",'Another gifts...?','birthday');
-// defaultEvents('2022-03-03', "MY LADY'S BIRTHDAY",'A lot of money to spent!!!!','birthday');
+// ------ управление функциями -------
 
 
-// ------ functions control -------
-
-// button of the current day
-// todayBtn.on("click", function() {
-//   if (month < indexMonth) {
-//     var step = indexMonth % month;
-//     movePrev(step, true);
-//   } else if (month > indexMonth) {
-//     var step = month - indexMonth;
-//     moveNext(step, true);
-//   }
-// });
-
-//higlight the cel of current day
-dataCel.each(function() {
+//высветите значение текущего дня
+dataCel.each(function () {
   if ($(this).data("day") === today) {
     $(this).addClass("isToday");
     fillEventSidebar($(this));
   }
 });
 
-//window event creator
-addBtn.on("click", function() {
+
+// Открываем текстовое поле при выборе праздничный день
+$('.form-control').change(function () {
+  if ($(this).val() === 'birthday') {
+    $('.text-notes').removeClass('display-none');
+  } else if (!$('.text-notes').hasClass('display-none')) {
+    $('.text-notes').addClass('display-none')
+  }
+})
+
+//создатель события
+addBtn.on("click", function () {
   winCreator.addClass("isVisible");
-  $("body").addClass("overlay");
-  dataCel.each(function() {
+  $(".panel").addClass("overlay");
+
+  dataCel.each(function () {
+
     if ($(this).hasClass("isSelected")) {
       today = $(this).data("day");
-      document.querySelector('input[type="date"]').value = today;
+      // console.log(today);
+      document.querySelector('input[name="date"]').value = today;
     } else {
-      document.querySelector('input[type="date"]').value = today;
+      document.querySelector('input[name="date"]').value = today;
     }
   });
+
+  $(function () {
+    $(".js-hide-search").select2(
+      {
+        dropdownParent: $('#addEvent'),
+        minimumResultsForSearch: 1 / 0,
+      });
+  });
+
 });
-closeBtn.on("click", function() {
+closeBtn.on("click", function () {
   winCreator.removeClass("isVisible");
-  $("body").removeClass("overlay");
+  $(".panel").removeClass("overlay");
+  if (!$('.text-notes').hasClass('display-none')) {
+    $('.text-notes').addClass('display-none');
+  }
 });
-saveBtn.on("click", function() {
-  var inputName = $("input[name=name]").val();
+saveBtn.on("click", function () {
   var inputDate = $("input[name=date]").val();
   var inputNotes = $("textarea[name=notes]").val();
   var inputTag = $("select[name=tags]")
     .find(":selected")
     .val();
-  
 
-  dataCel.each(function() {
+  dataCel.each(function () {
     if ($(this).data("day") === inputDate) {
-      if (inputName != null) {
-        $(this).attr("data-name", inputName);
-      }
       if (inputNotes != null) {
         $(this).attr("data-notes", inputNotes);
       }
       $(this).addClass("event");
       if (inputTag != null) {
-        if ($(this).hasClass('event')) 
-        $(this).addClass("event--" + inputTag);
+        $(this).attr("data-tags", inputTag);
+
+        if ($(this).hasClass("event--event")) {
+          $(this).removeClass("event--event");
+          $(this).addClass("event--" + inputTag);
+        } else if ($(this).hasClass("event--important")) {
+          $(this).removeClass("event--important");
+          $(this).addClass("event--" + inputTag);
+        } else if ($(this).hasClass("event--birthday")) {
+          $(this).removeClass("event--birthday");
+          $(this).addClass("event--" + inputTag);
+        } else {
+          $(this).addClass("event--" + inputTag);
+        }
+
       }
       fillEventSidebar($(this));
+      if (!$('.text-notes').hasClass('display-none')) {
+        $('.text-notes').addClass('display-none');
+      }
     }
   });
 
   winCreator.removeClass("isVisible");
-  $("body").removeClass("overlay");
+  $(".panel").removeClass("overlay");
   $("#addEvent")[0].reset();
+  $('.js-hide-search').select2('destroy');
+
 });
 
-//fill sidebar event info
+//заполнить боковую панель информацией о событии
 function fillEventSidebar(self) {
   $(".c-aside__event").remove();
-  var thisName = self.attr("data-name");
   var thisNotes = self.attr("data-notes");
   var thisImportant = self.hasClass("event--important");
   var thisBirthday = self.hasClass("event--birthday");
-  var thisFestivity = self.hasClass("event--festivity");
   var thisEvent = self.hasClass("event");
-  
+
   switch (true) {
     case thisImportant:
       $(".c-aside__eventList").append(
         "<p class='c-aside__event c-aside__event--important'>" +
-        thisName +
+        "Выходной день" +
         " <span> • " +
         thisNotes +
         "</span></p>"
@@ -153,16 +152,7 @@ function fillEventSidebar(self) {
     case thisBirthday:
       $(".c-aside__eventList").append(
         "<p class='c-aside__event c-aside__event--birthday'>" +
-        thisName +
-        " <span> • " +
-        thisNotes +
-        "</span></p>"
-      );
-      break;
-    case thisFestivity:
-      $(".c-aside__eventList").append(
-        "<p class='c-aside__event c-aside__event--festivity'>" +
-        thisName +
+        "Праздничный день" +
         " <span> • " +
         thisNotes +
         "</span></p>"
@@ -171,22 +161,23 @@ function fillEventSidebar(self) {
     case thisEvent:
       $(".c-aside__eventList").append(
         "<p class='c-aside__event'>" +
-        thisName +
+        "Рабочий день" +
         " <span> • " +
         thisNotes +
         "</span></p>"
       );
       break;
-   }
+  }
 };
-dataCel.on("click", function() {
+
+dataCel.on("click", function () {
   var thisEl = $(this);
   var thisDay = $(this)
-  .attr("data-day")
-  .slice(8);
+    .attr("data-day")
+    .slice(8);
   var thisMonth = $(this)
-  .attr("data-day")
-  .slice(5, 7);
+    .attr("data-day")
+    .slice(5, 7);
 
   fillEventSidebar($(this));
 
@@ -198,78 +189,114 @@ dataCel.on("click", function() {
 
 });
 
-//function for move the months
-function moveNext(fakeClick, indexNext) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "-=100%"
-    });
-    $(".c-paginator__month").css({
-      left: "-=100%"
-    });
-    switch (true) {
-      case indexNext:
-        indexMonth += 1;
-        break;
-    }
-  }
-}
-function movePrev(fakeClick, indexPrev) {
-  for (var i = 0; i < fakeClick; i++) {
-    $(".c-main").css({
-      left: "+=100%"
-    });
-    $(".c-paginator__month").css({
-      left: "+=100%"
-    });
-    switch (true) {
-      case indexPrev:
-        indexMonth -= 1;
-        break;
-    }
-  }
-}
-
-//months paginator
-function buttonsPaginator(buttonId, mainClass, monthClass, next, prev) {
-  switch (true) {
-    case next:
-      $(buttonId).on("click", function() {
-        if (indexMonth >= 2) {
-          $(mainClass).css({
-            left: "+=100%"
-          });
-          $(monthClass).css({
-            left: "+=100%"
-          });
-          indexMonth -= 1;
-        }
-        return indexMonth;
-      });
-      break;
-    case prev:
-      $(buttonId).on("click", function() {
-        if (indexMonth <= 11) {
-          $(mainClass).css({
-            left: "-=100%"
-          });
-          $(monthClass).css({
-            left: "-=100%"
-          });
-          indexMonth += 1;
-        }
-        return indexMonth;
-      });
-      break;
-  }
-}
-
-buttonsPaginator("#next", monthEl, ".c-paginator__month", false, true);
-buttonsPaginator("#prev", monthEl, ".c-paginator__month", true, false);
-
-//launch function to set the current month
-moveNext(indexMonth - 1, false);
-
-//fill the sidebar with current day
+//заполните боковую панель текущим днем
 $(".c-aside__num").text(day);
 $(".c-aside__month").text(monthText[month - 1]);
+
+//
+$('div.c-cal__data').each(function () {
+  if ($(this).attr('day-week') === 'Sunday' || $(this).attr('day-week') === 'Saturday') {
+    $(this).addClass('event');
+    $(this).addClass('event--important');
+    $(this).attr('data-notes', '');
+    $(this).attr('data-tags', 'important');
+  }
+  if ($(this).attr('day-week') !== 'Sunday' & $(this).attr('day-week') !== 'Saturday') {
+    $(this).addClass('event');
+    $(this).addClass('event--event');
+    $(this).attr('data-notes', '');
+    $(this).attr('data-tags', 'event');
+  }
+})
+
+//Определяем активный календарь
+$(`div.tabel-month-${year}`).addClass('active-year');
+
+
+
+// Кнопка сохранения всех элементов и значений
+function saveData() {
+  var data = [];
+  $('div.active-year > div.c-main > div.c-cal__row > div.c-cal__data').each(function () {
+    if (typeof $(this).attr('data-tags') === 'undefined' || $(this).attr('data-tags') === '') {
+      $(this).attr('data-tags', 'пуст');
+    }
+    if (typeof $(this).attr('data-notes') === 'undefined' || $(this).attr('data-notes') === '') {
+      $(this).attr('data-notes', 'пуст');
+    }
+
+    data.push([
+      $(this).attr('data-day'),
+      $(this).attr('data-tags'),
+      $(this).attr('data-notes'),
+    ])
+  })
+  console.log(data);
+}
+
+
+// Вперёд на 1 год
+function nextYear(year) {
+  $(`.tabel-month-${year}`).addClass('pos-abs');
+  $(`div.tabel-month-${year}`).removeClass('active-year');
+  const newYear = ++year;
+  $('.c-paginator__year').text(newYear);
+  $(`div.tabel-month-${newYear}`).addClass('active-year');
+
+  disabled(newYear);
+}
+// Назад на 1 год
+function prevYear(year) {
+  $(`div.tabel-month-${year}`).removeClass('active-year');
+  const newYear = --year;
+  $(`div.tabel-month-${newYear}`).addClass('active-year');
+  $(`.tabel-month-${newYear}`).removeClass('pos-abs');
+  $('.c-paginator__year').text(newYear);
+  disabled(newYear);
+}
+
+var yearFull = dateObj.getUTCFullYear();
+// Запрет на переключение года
+function disabled(year) {
+  if (year <= yearFull) {
+    $('.prev-year').addClass('disabled');
+    $('.prev-year').attr('disabled', true);
+  } else {
+    $('.prev-year').removeClass('disabled');
+    $('.prev-year').attr('disabled', false);
+  }
+
+  if (year >= yearFull + 1) {
+    $('.next-year').addClass('disabled');
+    $('.next-year').attr('disabled', true);
+  } else {
+    $('.next-year').removeClass('disabled');
+    $('.next-year').attr('disabled', false);
+  }
+}
+disabled($('.c-paginator__year').text())
+
+
+
+
+if (window.screen.width <= 640) {
+  console.log('Ширина экрана меньше 640px');
+  $('div.c-cal__data').each(function () {
+    $(this).on('click', function() {
+      $('.c-aside').addClass('active-aside');
+      $(".panel").addClass("overlay");
+
+      const closeBtn = document.createElement('div');
+      closeBtn.classList.add('close-aside');
+      document.querySelector('.active-aside').append(closeBtn);
+
+      addBtn.on("click", function () {
+        $('.c-aside').removeClass('active-aside');
+      })
+      closeBtn.addEventListener('click', () => {
+        $('.c-aside').removeClass('active-aside');
+        $(".panel").removeClass("overlay");
+      })
+    })
+  })
+}
